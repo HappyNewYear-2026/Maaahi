4357d719b273b5ea6074613e5fa7fd50e62812ad
your Ashish <hackz.talks@gmail.com>
2026-02-11 14:41:08 +0530

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react"
 import { motion, AnimatePresence } from "framer-motion"
-import StarryBackground from "@/components/StarryBackground"
+import BackgroundAnimation from "@/components/BackgroundAnimation"
 import OpeningPage from "@/components/OpeningPage"
 import Game from "@/components/Game"
 import DiaryPage from "@/components/DiaryPage"
